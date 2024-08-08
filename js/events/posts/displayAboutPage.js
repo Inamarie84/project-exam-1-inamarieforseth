b1e0f380eb083d592e2c6213a89422081ae61469
Inamarie84 <123692666+Inamarie84@users.noreply.github.com>
2024-08-08 20:02:46 +0200

    added some media styling

--- js/events/posts/displayAboutPage.js
+++ js/events/posts/displayAboutPage.js
@@ -1,34 +1,3 @@
-// import {
-//   fetchPostTitles,
-//   fetchImages,
-// } from "../../api/posts/fetchPostTitlesAbout.js";
-// import { renderPostTitles } from "../../ui/posts/renderPostTitles.js";
-// import { renderImages } from "../../ui/utilities/renderImages.js";
-
-// import { displayMessage } from "../../ui/common/displayMessage.js";
-
-// export function initializeAboutPage() {
-//   async function init() {
-//     try {
-//       const [titles, images] = await Promise.all([
-//         fetchPostTitles(),
-//         fetchImages(),
-//       ]);
-//       renderPostTitles(titles);
-//       renderImages(images);
-//     } catch (error) {
-//       console.error("Error initializing about page:", error);
-//       displayMessage(
-//         "#notification-message",
-//         "error",
-//         "Failed to load page content"
-//       );
-//     }
-//   }
-
-//   document.addEventListener("DOMContentLoaded", init);
-// }
-
 import {
   fetchPostTitles,
   fetchImages,
